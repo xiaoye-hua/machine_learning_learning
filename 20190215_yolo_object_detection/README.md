1e0f66023e9abb6d38f82f5dce61b510868f8f61
HuaGuo1992 <huag@kth.se>
2019-02-15 14:28:38 +0800

    add yolo-object-detection project

---
+++ 20190215_yolo_object_detection/README.md
@@ -0,0 +1,17 @@
+20190215
+
+![项目结构](../images/project_structure.png)
+
+文件说明:
+
+1. yolo.py 图片目标检测
+2. yolo_video.py 视频文件目标检测
+3. yolo_streaming.py  用安卓手机摄像头,实时目标检测 
+
+
+
+参考链接:
+
+1. [Darknet YOLOv3](https://pjreddie.com/darknet/yolo/)
+2. [Ubuntu 如何调用android摄像头](https://blog.csdn.net/ye_song/article/details/77249353)
+2. [YOLOv3 图片和离线视频目标检测](https://www.pyimagesearch.com/2018/11/12/yolo-object-detection-with-opencv/#)
---
+++ 20190215_yolo_object_detection/yolo_streaming.py
@@ -0,0 +1,153 @@
+
+# coding=utf-8
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os
+
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    # ap.add_argument("-i", "--input", required=True,
+    #                 help="path to input video")
+    # ap.add_argument("-o", "--output", required=True,
+    #                 help="path to output video")
+    ap.add_argument("-y", "--yolo", required=True,
+                    help="base path to YOLO directory")
+    ap.add_argument("-c", "--confidence", type=float, default=0.5,
+                    help="minimum probability to filter weak detections")
+    ap.add_argument("-t", "--threshold", type=float, default=0.3,
+                    help="threshold when applyong non-maxima suppression")
+    args = vars(ap.parse_args())
+
+    # load the COCO class labels our YOLO model was trained on
+    labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+    LABELS = open(labelsPath).read().strip().split("\n")
+
+    # initialize a list of colors to represent each possible class label
+    np.random.seed(42)
+    COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+                               dtype="uint8")
+
+    # derive the paths to the YOLO weights and model configuration
+    weightsPath = os.path.sep.join([args["yolo"], "yolov3-tiny.weights"])
+    configPath = os.path.sep.join([args["yolo"], "yolov3-tiny.cfg"])
+
+    # load our YOLO object detector trained on COCO dataset (80 classes)
+    # and determine only the *output* layer names that we need from YOLO
+    print("[INFO] loading YOLO from disk...")
+    net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    # initialize the video stream, pointer to output video file, and
+    # frame dimensions
+    # vs = cv2.VideoCapture(args["input"])
+
+
+
+    cv2.namedWindow("camera", 1)
+    # 开启ip摄像头
+    video = "http://admin:admin@192.168.1.100:8081/"  # 此处@后的ipv4 地址需要修改为自己的地址
+    vs = cv2.VideoCapture(video)
+    writer = None
+    (W, H) = (None, None)
+
+    num = 0
+    while True:
+        grabbed, frame = vs.read()
+        # of the stream
+        if not grabbed:
+            break
+
+        # if the frame dimensions are empty, grab them
+        if W is None or H is None:
+            (H, W) = frame.shape[:2]
+
+        # construct a blob from the input frame and then perform a forward
+        # pass of the YOLO object detector, giving us our bounding boxes
+        # and associated probabilities
+        blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+                                     swapRB=True, crop=False)
+        net.setInput(blob)
+        start = time.time()
+        layerOutputs = net.forward(ln)
+        end = time.time()
+
+        # initialize our lists of detected bounding boxes, confidences,
+        # and class IDs, respectively
+        boxes = []
+        confidences = []
+        classIDs = []
+
+        # loop over each of the layer outputs
+        for output in layerOutputs:
+            # loop over each of the detections
+            for detection in output:
+                # extract the class ID and confidence (i.e., probability)
+                # of the current object detection
+                scores = detection[5:]
+                classID = np.argmax(scores)
+                confidence = scores[classID]
+
+                # filter out weak predictions by ensuring the detected
+                # probability is greater than the minimum probability
+                if confidence > args["confidence"]:
+                    # scale the bounding box coordinates back relative to
+                    # the size of the image, keeping in mind that YOLO
+                    # actually returns the center (x, y)-coordinates of
+                    # the bounding box followed by the boxes' width and
+                    # height
+                    box = detection[0:4] * np.array([W, H, W, H])
+                    (centerX, centerY, width, height) = box.astype("int")
+
+                    # use the center (x, y)-coordinates to derive the top
+                    # and and left corner of the bounding box
+                    x = int(centerX - (width / 2))
+                    y = int(centerY - (height / 2))
+
+                    # update our list of bounding box coordinates,
+                    # confidences, and class IDs
+                    boxes.append([x, y, int(width), int(height)])
+                    confidences.append(float(confidence))
+                    classIDs.append(classID)
+
+        # apply non-maxima suppression to suppress weak, overlapping
+        # bounding boxes
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
+                                args["threshold"])
+
+        # ensure at least one detection exists
+        if len(idxs) > 0:
+            # loop over the indexes we are keeping
+            for i in idxs.flatten():
+                # extract the bounding box coordinates
+                (x, y) = (boxes[i][0], boxes[i][1])
+                (w, h) = (boxes[i][2], boxes[i][3])
+
+                # draw a bounding box rectangle and label on the frame
+                color = [int(c) for c in COLORS[classIDs[i]]]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                text = "{}: {:.4f}".format(LABELS[classIDs[i]],
+                                           confidences[i])
+                cv2.putText(frame, text, (x, y - 5),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        # print(end-start)
+        cv2.imshow("camera", frame)
+        # 按键处理，注意，焦点应当在摄像头窗口，不是在终端命令行窗口
+        key = cv2.waitKey(10)
+        if key == 27:
+            # esc键退出
+            print("esc break...")
+            break
+        if key == ord(' '):
+            # 保存一张图像
+            num = num + 1
+            filename = "frames_%s.jpg" % num
+            cv2.imwrite(filename, frame)
+
+    vs.release()
+    cv2.destroyWindow("camera")
